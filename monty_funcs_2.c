1d8cb62d10b7294f6e06d087da003efe154ce2be
Otmanbboy <otmanbboy@gmail.com>
2023-09-20 23:27:50 +0100

    create monty_mod fnct

--- monty_funcs_2.c
+++ monty_funcs_2.c
@@ -4,6 +4,7 @@ void monty_add(stack_t **stack, unsigned int line_number);
 void monty_sub(stack_t **stack, unsigned int line_number);
 void monty_div(stack_t **stack, unsigned int line_number);
 void monty_mul(stack_t **stack, unsigned int line_number);
+void monty_mod(stack_t **stack, unsigned int line_number);
 
 /**
  * monty_add - Adds the top two values of a stack_t linked list.
@@ -76,7 +77,7 @@ void monty_div(stack_t **stack, unsigned int line_number)
 /**
  * monty_mul - Multiplies the second value from the top of
  *             a stack_t linked list by the top value.
- * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @stack: pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
  *
  * Description: The result is stored in the second value node
@@ -91,5 +92,32 @@ void monty_mul(stack_t **stack, unsigned int line_number)
 	}
 
 	(*stack)->next->next->n *= (*stack)->next->n;
+	monty_pop(stack, line_number);
+}
+
+/**
+ * monty_mod - Computes the modulus of the second value from the
+ *             top of a stack_t linked list  by the top value.
+ * @stack: pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ *
+ * Description: The result is stored in the second value node
+ *              from the top and the top value is removed.
+ */
+void monty_mod(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "mod"));
+		return;
+	}
+
+	if ((*stack)->next->n == 0)
+	{
+		set_op_tok_error(div_error(line_number));
+		return;
+	}
+
+	(*stack)->next->next->n %= (*stack)->next->n;
 	monty_pop(stack, line_number);
 }
