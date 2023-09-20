832cb67391d790d3e28d7a98178dc845fddc6570
amxsupport <itmasterdesigne@gmail.com>
2023-09-20 21:45:33 +0000

    implement main function

--- monty_main.c
+++ monty_main.c
@@ -20,6 +20,16 @@ char **op_toks = NULL;
  */
 int main(int argc, char **argv)
 {
-	/* implementation */
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
+
+	if (argc != 2)
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
 }
 
