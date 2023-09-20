72c67fd6e04dd40b4c5e27506555bf60a3cdb626
Otmanbboy <otmanbboy@gmail.com>
2023-09-20 23:09:46 +0100

    create monty_pint fnct

--- monty_funcs_1.c
+++ monty_funcs_1.c
@@ -2,6 +2,7 @@
 
 void monty_push(stack_t **stack, unsigned int line_number);
 void monty_pall(stack_t **stack, unsigned int line_number);
+void monty_pint(stack_t **stack, unsigned int line_number);
 
 /**
  * monty_push - Pushes a value to a stack_t linked list.
@@ -73,4 +74,20 @@ void monty_pall(stack_t **stack, unsigned int line_number)
 		tmp = tmp->next;
 	}
 	(void)line_number;
+}
+
+/**
+ * monty_pint - Prints the top value of a stack_t linked list.
+ * @stack: pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pint(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pint_error(line_number));
+		return;
+	}
+
+	printf("%d\n", (*stack)->next->n);
 }
