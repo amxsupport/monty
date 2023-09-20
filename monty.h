56de3530bae3be6d008a37d2ba9a44247d50f6af
Otmanbboy <otmanbboy@gmail.com>
2023-09-20 23:22:14 +0100

    create monty_sub fnct

--- monty.h
+++ monty.h
@@ -58,5 +58,7 @@ void monty_pint(stack_t **stack, unsigned int line_number);
 void monty_pop(stack_t **stack, unsigned int line_number);
 void monty_swap(stack_t **stack, unsigned int line_number);
 void monty_add(stack_t **stack, unsigned int line_number);
+void monty_sub(stack_t **stack, unsigned int line_number);
+
 
 #endif /* __MONTY_H__ */
