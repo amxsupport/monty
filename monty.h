ed2c4dc53fcde6ced4930888742c05ff0614025f
Otmanbboy <otmanbboy@gmail.com>
2023-09-20 23:19:58 +0100

    create monty_add fnct

--- monty.h
+++ monty.h
@@ -57,5 +57,6 @@ void monty_pall(stack_t **stack, unsigned int line_number);
 void monty_pint(stack_t **stack, unsigned int line_number);
 void monty_pop(stack_t **stack, unsigned int line_number);
 void monty_swap(stack_t **stack, unsigned int line_number);
+void monty_add(stack_t **stack, unsigned int line_number);
 
 #endif /* __MONTY_H__ */
