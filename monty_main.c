3307dae80f1d551a694f77cd9f19ed2b61442e20
amxsupport <itmasterdesigne@gmail.com>
2023-09-20 21:44:29 +0000

    create monty_main.c

---
+++ monty_main.c
@@ -0,0 +1,25 @@
+/*
+ * File: monty_main.c
+ *
+ */
+
+#include "monty.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char **op_toks = NULL;
+
+/**
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ */
+int main(int argc, char **argv)
+{
+	/* implementation */
+}
+
