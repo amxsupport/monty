1f12c2897ac4966ac90c168c89e1400d00a495d0
amxsupport <itmasterdesigne@gmail.com>
2023-09-20 20:11:08 +0000

    create run_monty.c and implement functions

---
+++ run_monty.c
@@ -0,0 +1,66 @@
+/*
+ * File: run_monty.c
+ */
+
+#include "monty.h"
+#include <string.h>
+
+void free_tokens(void);
+unsigned int token_arr_len(void);
+int is_empty_line(char *line, char *delims);
+void (*get_op_func(char *opcode))(stack_t**, unsigned int);
+int run_monty(FILE *script_fd);
+
+/**
+ * free_tokens - Frees the global op_toks array of strings.
+ */
+void free_tokens(void)
+{
+	/* implement free_tokens */
+}
+
+/**
+ * token_arr_len - Gets the length of current op_toks.
+ *
+ * Return: Length of current op_toks (as int).
+ */
+unsigned int token_arr_len(void)
+{
+	  /* implement free_tokens */
+}
+/**
+ * is_empty_line - Checks if a line read from getline only contains delimiters.
+ * @line: A pointer to the line.
+ * @delims: A string of delimiter characters.
+ *
+ * Return: If the line only contains delimiters - 1.
+ *         Otherwise - 0.
+ */
+int is_empty_line(char *line, char *delims)
+{
+	 /* implement free_tokens */
+}
+
+/**
+ * get_op_func - Matches an opcode with its corresponding function.
+ * @opcode: The opcode to match.
+ *
+ * Return: A pointer to the corresponding function.
+ */
+void (*get_op_func(char *opcode))(stack_t**, unsigned int)
+{
+	 /* implement free_tokens */
+}
+
+
+/**
+ * run_monty - Primary function to execute a Monty bytecodes script.
+ * @script_fd: File descriptor for an open Monty bytecodes script.
+ *
+ * Return: EXIT_SUCCESS on success, respective error code on failure.
+ */
+int run_monty(FILE *script_fd)
+{
+	/* implement free_tokens */
+}
+
