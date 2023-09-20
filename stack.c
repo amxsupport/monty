6f68f0f59e3b873920aa9d23cf3e4adab2529cd3
amxsupport <itmasterdesigne@gmail.com>
2023-09-20 20:41:59 +0000

    file : stach.c , implementation of function check_mode

--- stack.c
+++ stack.c
@@ -62,5 +62,9 @@ int init_stack(stack_t **stack)
  */
 int check_mode(stack_t *stack)
 {
-	/* implement check_mode */
+	if (stack->n == STACK)
+		return (STACK);
+	else if (stack->n == QUEUE)
+		return (QUEUE);
+	return (2);
 }
