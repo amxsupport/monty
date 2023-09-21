cbf9bfcc8f6e55e0f2c2e0e835fd81c05b1629ff
amxsupport <itmasterdesigne@gmail.com>
2023-09-21 21:32:04 +0000

    update monty.h

--- monty.h
+++ monty.h
@@ -70,6 +70,10 @@ void monty_rotr(stack_t **stack, unsigned int line_number);
 void monty_stack(stack_t **stack, unsigned int line_number);
 void monty_queue(stack_t **stack, unsigned int line_number);
 
+/* CUSTOM STANDARD LIBRARY FUNCTIONS */
+char **strtow(char *str, char *delims);
+char *get_int(int n);
+
 /* ERROR MESSAGES & ERROR CODES */
 int usage_error(void);
 int malloc_error(void);
