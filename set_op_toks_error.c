bb176dedd6b90d7bff39496ed08ae81b1a93eeed
amxsupport <itmasterdesigne@gmail.com>
2023-09-21 20:47:26 +0000

    create set_op_toks_error.c file

---
+++ set_op_toks_error.c
@@ -0,0 +1,14 @@
+/*
+ * File: set_op_tok_error.c
+ */
+
+#include "monty.h"
+
+/**
+ * set_op_tok_error - Sets last element of op_toks to be an error code.
+ * @error_code: Integer to store as a string in op_toks.
+ */
+void set_op_tok_error(int error_code)
+{
+
+}
