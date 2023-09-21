f78bab59b76d9c5a4b1486404eb96c576807e695
amxsupport <itmasterdesigne@gmail.com>
2023-09-21 20:57:24 +0000

    update tokenize_string.c file implementation of strtow function

--- tokenize_string.c
+++ tokenize_string.c
@@ -21,7 +21,49 @@ char *get_next_word(char *str, char *delims);
 
 char **strtow(char *str, char *delims)
 {
-	/* implementation */
+	char **words = NULL;
+	int wc, wordLen, n, i = 0;
+
+	if (str == NULL || !*str)
+		return (NULL);
+	wc = get_word_count(str, delims);
+
+
+	if (wc == 0)
+		return (NULL);
+	words = malloc((wc + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	while (i < wc)
+	{
+		wordLen = get_word_length(str, delims);
+		if (is_delim(*str, delims))
+		{
+			str = get_next_word(str, delims);
+		}
+		words[i] = malloc((wordLen + 1) * sizeof(char));
+		if (words[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				i--;
+				free(words[i]);
+			}
+			free(words);
+			return (NULL);
+		}
+		n = 0;
+		while (n < wordLen)
+		{
+			words[i][n] = *(str + n);
+			n++;
+		}
+		words[i][n] = '\0'; /* set end of str */
+		str = get_next_word(str, delims);
+		i++;
+	}
+	words[i] = NULL; /* last element is null for iteration */
+	return (words);
 }
 
 /**
