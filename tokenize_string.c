833acd18f7268b2d389e2d73b92c5886ff7370ab
amxsupport <itmasterdesigne@gmail.com>
2023-09-21 20:56:00 +0000

    create tokenize_string.c file

---
+++ tokenize_string.c
@@ -0,0 +1,82 @@
+/*
+ * File: tokenize_string.c
+ */
+
+#include <stdlib.h>
+
+char **strtow(char *str, char *delims);
+int is_delim(char ch, char *delims);
+int get_word_length(char *str, char *delims);
+int get_word_count(char *str, char *delims);
+char *get_next_word(char *str, char *delims);
+
+/**
+ * strtow - takes a string and seperates its words
+ *
+ * @str: string to seperate into words
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: 2D array of pointers to each word
+ */
+
+char **strtow(char *str, char *delims)
+{
+	/* implementation */
+}
+
+/**
+ * is_delim - checks if stream has delimitor char
+ *
+ * @ch: character in stream
+ *
+ * @delims: Pointer to null terminated array of delimitors
+ *
+ * Return: 1 (success) 0 (failure)
+ */
+
+int is_delim(char ch, char *delims)
+{
+	/* implementation */
+}
+
+/**
+ * get_word_length - gets the word length of cur word in str
+ *
+ * @str: string to get word length from current word
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: word length of current word
+ */
+
+int get_word_length(char *str, char *delims)
+{
+	/* implementation */
+}
+
+/**
+ * get_word_count - gets the word count of a string
+ *
+ * @str: string to get word count from
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: the word count of the string
+ */
+
+int get_word_count(char *str, char *delims)
+{
+	/* implementation */
+}
+
+/**
+ * get_next_word - gets the next word in a string
+ *
+ * @str: string to get next word from
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: pointer to first char of next word
+ */
+
+char *get_next_word(char *str, char *delims)
+{
+	 /* implementation */
+}
