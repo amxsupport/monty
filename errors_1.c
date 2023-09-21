764f1e8e1d89d07b47fdb7f2d7e242bf59626693
Otmanbboy <otmanbboy@gmail.com>
2023-09-21 14:02:32 +0100

    create no_int_error fnct

--- errors_1.c
+++ errors_1.c
@@ -4,7 +4,7 @@ int usage_error(void);
 int malloc_error(void);
 int f_open_error(char *filename);
 int unknown_op_error(char *opcode, unsigned int line_number);
-
+int no_int_error(unsigned int line_number);
 
 /**
  * usage_error - Prints usage error messages.
@@ -51,5 +51,17 @@ int unknown_op_error(char *opcode, unsigned int line_number)
 {
 	fprintf(stderr, "L%u: unknown instruction %s\n",
 		line_number, opcode);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * no_int_error - Prints invalid monty_push argument error messages.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int no_int_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
 	return (EXIT_FAILURE);
 }
