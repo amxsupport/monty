db65cf5dbb317b5f74997219b29b49855f6bda57
amxsupport <itmasterdesigne@gmail.com>
2023-09-21 20:37:39 +0000

    update int_to_string.c implement get_int function

--- int_to_string.c
+++ int_to_string.c
@@ -19,7 +19,25 @@ void fill_numbase_buff(unsigned int num, unsigned int base,
  */
 char *get_int(int num)
 {
-	/* implementation */
+	unsigned int temp;
+	int length = 0;
+	long num_l = 0;
+	char *ret;
+
+	temp = _abs(num);
+	length = get_numbase_len(temp, 10);
+
+	if (num < 0 || num_l < 0)
+		length++; /* negative sign */
+	ret = malloc(length + 1); /* create new string */
+	if (!ret)
+		return (NULL);
+
+	fill_numbase_buff(temp, 10, ret, length);
+	if (num < 0 || num_l < 0)
+		ret[0] = '-';
+
+	return (ret);
 }
 
 /**
