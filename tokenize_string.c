ee60962ca4241510aac0e90766fce5deda955804
amxsupport <itmasterdesigne@gmail.com>
2023-09-21 20:59:28 +0000

    update tokenize_string.c file implementation of get_word_length function

--- tokenize_string.c
+++ tokenize_string.c
@@ -100,7 +100,21 @@ int is_delim(char ch, char *delims)
 
 int get_word_length(char *str, char *delims)
 {
-	/* implementation */
+	int wLen = 0, pending = 1, i = 0;
+
+	while (*(str + i))
+	{
+		if (is_delim(str[i], delims))
+			pending = 1;
+		else if (pending)
+		{
+			wLen++;
+		}
+		if (wLen > 0 && is_delim(str[i], delims))
+			break;
+		i++;
+	}
+	return (wLen);
 }
 
 /**
