4867fd21d61a98b96ca26ac41d6f8933fc4cbd5e
Otmanbboy <otmanbboy@gmail.com>
2023-09-21 13:37:41 +0100

    create monty_pchar and pstr fnct

--- monty_funcs_3.c
+++ monty_funcs_3.c
@@ -2,6 +2,7 @@
 
 void monty_nop(stack_t **stack, unsigned int line_number);
 void monty_pchar(stack_t **stack, unsigned int line_number);
+void monty_pstr(stack_t **stack, unsigned int line_number);
 
 /**
  * monty_nop - Does absolutely nothing for the Monty opcode 'nop'.
@@ -35,4 +36,24 @@ void monty_pchar(stack_t **stack, unsigned int line_number)
 	}
 
 	printf("%c\n", (*stack)->next->n);
+}
+
+/**
+ * monty_pstr - Prints the string contained in a stack_t linked list.
+ * @stack: pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp && tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
+	{
+		printf("%c", tmp->n);
+		tmp = tmp->next;
+	}
+
+	printf("\n");
+
+	(void)line_number;
 }
