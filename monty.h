6af88089b736c4726bcf2fd66a80ce861c11313b
Otmanbboy <otmanbboy@gmail.com>
2023-09-21 13:43:23 +0100

    create monty_stack fnct

--- monty.h
+++ monty.h
@@ -67,6 +67,7 @@ void monty_pchar(stack_t **stack, unsigned int line_number);
 void monty_pstr(stack_t **stack, unsigned int line_number);
 void monty_rotl(stack_t **stack, unsigned int line_number);
 void monty_rotr(stack_t **stack, unsigned int line_number);
+void monty_stack(stack_t **stack, unsigned int line_number);
 
 
 #endif /* __MONTY_H__ */
--- monty_funcs_4.c
+++ monty_funcs_4.c
@@ -2,6 +2,7 @@
 
 void monty_rotl(stack_t **stack, unsigned int line_number);
 void monty_rotr(stack_t **stack, unsigned int line_number);
+void monty_stack(stack_t **stack, unsigned int line_number);
 
 
 /**
@@ -53,5 +54,16 @@ void monty_rotr(stack_t **stack, unsigned int line_number)
 	bottom->next = top;
 	top->prev = bottom;
 
+	(void)line_number;
+}
+
+/**
+ * monty_stack - Converts a queue to a stack.
+ * @stack: pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_stack(stack_t **stack, unsigned int line_number)
+{
+	(*stack)->n = STACK;
 	(void)line_number;
 }
